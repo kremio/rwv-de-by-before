043ad868a13d08d64208b78f167bc92b9b5f2da6
Kremio Software <kremio.software@gmail.com>
2018-10-14 18:40:49 +0200

    Validate report scraper against schema

--- tests/lib/report.test.js
+++ tests/lib/report.test.js
@@ -1,23 +1,26 @@
 const fs = require('fs')
 const request = require('request')
+const validateSchema = require('rwv-schema')
 const scrapeReport = require('../../lib/report')
 
 const reportJson = require('../samples/report.json')
 //Mock HTTP requests
 jest.mock('request')
 
-
-
+//Use a pre-fetched sample report
+const sampleReport = fs.readFileSync('./tests/samples/report.html', 'utf8')
+request.mockImplementation((...args) => {
+  const cb = args.pop()
+  cb( null, {statusCode: 200}, sampleReport )
+})
 
 
 test( 'Base test', async () => {
-  //Use a pre-fetched sample report
-  const sampleReport = fs.readFileSync('./tests/samples/report.html', 'utf8')
-  request.mockImplementation((...args) => {
-    const cb = args.pop()
-    cb( null, {statusCode: 200}, sampleReport )
-  })
-
   const result = await scrapeReport( 'https://domain.tld/path/to/page.html' )
   expect( result ).toEqual( reportJson )
+})
+
+test( 'Validate against schema', async () => {
+  const result = await scrapeReport( 'https://domain.tld/path/to/page.html' )
+  expect( validateSchema(result) ).toBeTruthy()
 })
