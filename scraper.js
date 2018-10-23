9c321fd2d001895674e17d8d51f9369f69b12c96
Kremio Software <kremio.software@gmail.com>
2018-10-23 13:39:57 +0200

    Scraper v 0.0.9

--- scraper.js
+++ scraper.js
@@ -1,1 +1,27 @@
-console.log("Scrapper executed in "+process.env.NODE_ENV)
+//NOTE: When morph.io executes the scraper, it sets process.env.NODE_ENV
+//to 'production'
+const path = require('path')
+const getDB = require('rwv-sqlite/lib/db')
+const InsertStream = require('rwv-sqlite/lib/stream')
+//const {launch,JSONToString} = require('rwv-sqlite')
+const scrape = require('./scrape')
+
+//Setup the database
+const {DB, migrations} = getDB( path.resolve('./config/database.json') )
+
+//Get the last inserted report, if any
+new Promise( (s,f) => {
+  DB.db.get('SELECT uri FROM data ORDER BY createdDate LIMIT 1', (err, row) => {
+    if(err){
+      f(err)
+      return
+    }
+    s(row ? row.uri : false)
+  })
+}).then( (stopAtReportURI) => {
+  //Scrape and insert
+  const insertStream = new InsertStream({}, DB)
+  scrape({stopAtReportURI}).pipe(insertStream)
+}).catch( (e) => {
+  console.error(e)
+})
